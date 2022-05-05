517653c03aaf9dad7f0d6a5392d6688089da019a
david <davidsu@i2dsp.cn>
2022-05-05 14:53:10 +0800

    feat:完成pexels图片详情页下载图片的功能。

---
+++ lib/_demos/image_gallery_saver_demo.dart
@@ -0,0 +1,169 @@
+// ignore_for_file: prefer_const_constructors, avoid_print
+
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Save image to gallery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey _globalKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _requestPermission();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Save image to gallery"),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              RepaintBoundary(
+                key: _globalKey,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.red,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: _saveScreen,
+                  child: Text("Save Local Image"),
+                ),
+                width: 200,
+                height: 44,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: _getHttp,
+                  child: Text("Save network image"),
+                ),
+                width: 200,
+                height: 44,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: _saveVideo,
+                  child: Text("Save network video"),
+                ),
+                width: 200,
+                height: 44,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: _saveGif,
+                  child: Text("Save Gif to gallery"),
+                ),
+                width: 200,
+                height: 44,
+              ),
+            ],
+          ),
+        ));
+  }
+
+  _requestPermission() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.storage,
+    ].request();
+
+    final info = statuses[Permission.storage].toString();
+    print(info);
+    _toastInfo(info);
+  }
+
+  _saveScreen() async {
+    RenderRepaintBoundary boundary =
+        _globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? byteData =
+        await (image.toByteData(format: ui.ImageByteFormat.png));
+    if (byteData != null) {
+      final result =
+          await ImageGallerySaver.saveImage(byteData.buffer.asUint8List());
+      print(result);
+      _toastInfo(result.toString());
+    }
+  }
+
+  _getHttp() async {
+    var response = await Dio().get(
+        "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg",
+        options: Options(responseType: ResponseType.bytes));
+    final result = await ImageGallerySaver.saveImage(
+        Uint8List.fromList(response.data),
+        quality: 60,
+        name: "hello");
+    print(result);
+    _toastInfo("$result");
+  }
+
+  _saveGif() async {
+    var appDocDir = await getTemporaryDirectory();
+    String savePath = appDocDir.path + "/temp.gif";
+    String fileUrl =
+        "https://hyjdoc.oss-cn-beijing.aliyuncs.com/hyj-doc-flutter-demo-run.gif";
+    await Dio().download(fileUrl, savePath);
+    final result = await ImageGallerySaver.saveFile(savePath);
+    print(result);
+    _toastInfo("$result");
+  }
+
+  _saveVideo() async {
+    var appDocDir = await getTemporaryDirectory();
+    String savePath = appDocDir.path + "/temp.mp4";
+    String fileUrl =
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4";
+    await Dio().download(fileUrl, savePath, onReceiveProgress: (count, total) {
+      print((count / total * 100).toStringAsFixed(0) + "%");
+    });
+    final result = await ImageGallerySaver.saveFile(savePath);
+    print(result);
+    _toastInfo("$result");
+  }
+
+  _toastInfo(String info) {
+    Fluttertoast.showToast(msg: info, toastLength: Toast.LENGTH_LONG);
+  }
+}
--- lib/views/pexels_image_widget/pexels_image_detail_page.dart
+++ lib/views/pexels_image_widget/pexels_image_detail_page.dart
@@ -1,48 +1,95 @@
+// ignore_for_file: avoid_print
+
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:freader/models/pexels_api_images_result.dart';
 import 'package:freader/views/readhub_category/readhub_common_widgets.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 /// 点击指定图片跳转的详情页
-class PexelsImageDetailPage extends StatelessWidget {
+class PexelsImageDetailPage extends StatefulWidget {
   final PhotosData photoData;
   const PexelsImageDetailPage({Key? key, required this.photoData})
       : super(key: key);
 
   @override
+  _PexelsImageDetailPageState createState() => _PexelsImageDetailPageState();
+}
+
+class _PexelsImageDetailPageState extends State<PexelsImageDetailPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // 获取存取权限
+    _requestPermission();
+  }
+
+  // 是否在下载中（true显示一个滚动进度条，false则不显示）
+  var downloading = false;
+
+  @override
   Widget build(BuildContext context) {
-    var src = photoData.src?.medium;
+    var pd = widget.photoData;
+    var viewSrc = pd.src?.medium;
+    // 下载使用原图途径和pexels的该图片id作为名称
+    var downloadSrc = pd.src?.original;
+    var photoName = "pexels_${pd.id}";
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('照片详情 ✌️'),
+        title: const Text('照片详情'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          AspectRatio(
-            aspectRatio: 1,
-            child: SizedBox(
-              width: double.infinity,
-              child: src != ""
-                  ? Image(
-                      image: NetworkImage("$src"),
-                    )
-                  : const Icon(Icons.no_accounts),
-            ),
-          ),
-          Text(
-            "描述： ${photoData.alt}",
-            maxLines: 3,
-            style: TextStyle(
-              fontSize: 14.sp,
-              height: 1.2,
+          Center(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: SizedBox(
+                width: double.infinity,
+                child: viewSrc != ""
+                    ? Image(
+                        image: NetworkImage("$viewSrc"),
+                      )
+                    : const Icon(Icons.no_accounts),
+              ),
             ),
           ),
           Container(
             margin: EdgeInsets.all(5.0.sp),
             child: Text(
-              "作者：${photoData.photographer}",
+              "描述： ${pd.alt}",
+              maxLines: 3,
+              style: TextStyle(
+                fontSize: 14.sp,
+                height: 1.2,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(5.0.sp),
+            child: Text(
+              "作者：${pd.photographer}",
               style: TextStyle(fontSize: 14.sp),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(5.0.sp),
+            child: Text(
+              "温馨提醒：下载默认为高清大图，注意流量消耗。",
+              maxLines: 3,
+              style: TextStyle(
+                fontSize: 14.sp,
+                height: 1.2,
+              ),
             ),
           ),
           // 预留的照片详情的操作按鈕
@@ -54,7 +101,7 @@ class PexelsImageDetailPage extends StatelessWidget {
                 tooltip: "share",
                 child: Icon(
                   Icons.share,
-                  size: 14.sp,
+                  size: 20.sp,
                 ),
               ),
               SmallButtonWidget(
@@ -62,21 +109,74 @@ class PexelsImageDetailPage extends StatelessWidget {
                 tooltip: "star",
                 child: Icon(
                   Icons.star,
-                  size: 14.sp,
+                  size: 20.sp,
                 ),
               ),
               SmallButtonWidget(
-                onTap: () => {},
+                onTap: () => _downloadImage(downloadSrc!, photoName),
                 tooltip: "download",
                 child: Icon(
                   Icons.download,
-                  size: 14.sp,
+                  color: Colors.lightBlue,
+                  size: 20.sp,
                 ),
               ),
             ],
-          )
+          ),
+          // 照片下载进度条
+          downloading
+              ? LinearProgressIndicator(
+                  backgroundColor: Colors.grey[200],
+                  valueColor: const AlwaysStoppedAnimation(Colors.blue),
+                )
+              : Container(),
         ],
       ),
     );
+  }
+
+  // 下载图片到相册（默认Download文件夹）
+  _downloadImage(String url, String name) async {
+    _toastInfo("开始下载图片……");
+    setState(() {
+      downloading = true;
+    });
+
+    var appDocDir = await getTemporaryDirectory();
+    String savePath = appDocDir.path;
+
+    print("savePath $savePath");
+
+    var response = await Dio().get(
+      url,
+      options: Options(responseType: ResponseType.bytes),
+    );
+    final result = await ImageGallerySaver.saveImage(
+      Uint8List.fromList(response.data),
+      quality: 100,
+      name: name,
+    );
+    print(result);
+    // _toastInfo("$result");
+    _toastInfo("下载完成，请到相册查看");
+    setState(() {
+      downloading = false;
+    });
+  }
+
+  // 查看和请求存储权限
+  _requestPermission() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.storage,
+    ].request();
+
+    final info = statuses[Permission.storage].toString();
+    print(info);
+    // _toastInfo(info);
+  }
+
+  // 透明小弹窗显示信息
+  _toastInfo(String info) {
+    Fluttertoast.showToast(msg: info, toastLength: Toast.LENGTH_LONG);
   }
 }
